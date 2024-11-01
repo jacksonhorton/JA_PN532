5b8525eb32bcb959b3004b0ec5c936076f9a8ffc
Jackson Horton <jacksonhorton12@icloud.com>
2024-11-01 08:23:23 -0500

    Changed type byte to uint8.

--- src/NfcAdapter.h
+++ src/NfcAdapter.h
@@ -37,7 +37,7 @@ class NfcAdapter {
   boolean clean();
  private:
   JA_PN532* shield;
-  byte uid[7];  // Buffer to store the returned UID
+  uint8_t uid[7];  // Buffer to store the returned UID
   unsigned int uidLength; // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
   unsigned int guessTagType();
 };
