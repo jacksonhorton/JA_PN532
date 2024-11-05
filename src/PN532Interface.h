2fb29fb36e7d2d8c71285d4880838cfdb23e5431
Jackson Horton <jacksonhorton12@icloud.com>
2024-11-05 16:50:00 -0600

    Temporarily removed debug class's output functions.

--- src/PN532Interface.h
+++ src/PN532Interface.h
@@ -21,8 +21,8 @@
 #define PN532_NO_SPACE                (-4)
 
 #define REVERSE_BITS_ORDER(b)         b = (b & 0xF0) >> 4 | (b & 0x0F) << 4; \
-  b = (b & 0xCC) >> 2 | (b & 0x33) << 2;				\
-  b = (b & 0xAA) >> 1 | (b & 0x55) << 1
+                                      b = (b & 0xCC) >> 2 | (b & 0x33) << 2; \
+                                      b = (b & 0xAA) >> 1 | (b & 0x55) << 1
 
 class PN532Interface
 {
